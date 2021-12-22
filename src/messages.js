f3dce9409c11005f65e1afb94cc6f6458bf2a217
tengkuzulfadli <nomad.plus@yahoo.com>
2021-12-22 21:51:58 +1100

    Set up live chat app

---
+++ src/messages.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+
+function Messages() {
+  let [messages, setMessages] = useState([]);
+
+  useEffect(function () {
+    supabase
+      .from("messages")
+      .select()
+      .then(function (data) {
+        setMessages(data.body);
+      });
+  }, []);
+
+  useEffect(
+    function () {
+      supabase
+        .from("messages")
+        .on("INSERT", function (payload) {
+          // Spread Operator
+          // Pass in a new array with all of the current messages
+          // As well as the new message at the end
+          setMessages([...messages, payload.new]);
+        })
+        .subscribe();
+    },
+    [messages],
+  );
+
+  let messagesMarkup = messages.map(function (msg) {
+    return (
+      <div key={msg.id}>
+        <p>{msg.content}</p>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      {messagesMarkup}
+      <h2>SHOW THE FORM HERE</h2>
+    </div>
+  );
+}
+
+export default Messages;
